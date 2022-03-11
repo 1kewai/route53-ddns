ecc8f2965bf80568f644678be38ec9fb4f0d1a72
1kewai <yuto.ike.17@gmail.com>
2022-03-11 23:31:49 +0900

    コアな機能を完成

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import boto3
 import requests
+import auth
 
 def check_my_ip() -> str:
     try:
@@ -10,4 +11,45 @@ def check_my_ip() -> str:
         print("Error occured whie fetching global IP : {}".format(str(e)))
         return "0"
 
-check_my_ip()
+def create_aws_record(zoneid :str, domain :str, record :str, type :str) -> None:
+    client = boto3.client(
+        'route53',
+        aws_access_key_id=auth.aws_access_key_id,
+        aws_secret_access_key=auth.aws_access_secret,
+        region_name="ap-northeast-1"
+    )
+
+    client = boto3.client(
+        'route53',
+        aws_access_key_id=auth.aws_access_key_id,
+        aws_secret_access_key=auth.aws_access_secret,
+        region_name="ap-northeast-1"
+    )
+    client.change_resource_record_sets(
+    HostedZoneId=zoneid,
+    ChangeBatch={
+        'Comment': 'AutomaticDDNS',
+        'Changes': [
+            {
+                'Action': 'UPSERT',
+                'ResourceRecordSet': {
+                    'Name': domain,
+                    'Type': type,
+                    'TTL': 30,
+                    'ResourceRecords': [
+                        {
+                            'Value': record
+                        },
+                    ],
+                }
+            },
+        ]
+    }
+)
+
+def main_job():
+    ip = check_my_ip()
+    for domain in auth.domains:
+        create_aws_record(auth.zoneid, domain, ip, "A")
+
+main_job()
